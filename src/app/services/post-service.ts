782f33ab7072f143866f3c97feac665c8548ffc9
Prajwal Sharma <prjwl4@gmail.com>
2025-12-27 21:02:38 +0530

    feat: create post service

---
+++ src/app/services/post-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IPost } from '../models/ipost';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com;';
+
+  getAllPosts = () => {
+    return this.http.get<IPost[]>(this.apiUrl + 'posts');
+  };
+
+  getPost = (postId: number) => {
+    return this.http.get<IPost>(`${this.apiUrl}/posts/${postId}`);
+  };
+
+  createPost = (payload: IPost) => {
+    return this.http.post<IPost>(`${this.apiUrl}/posts`, payload);
+  };
+
+  updatePost = (payload: IPost) => {
+    return this.http.put<IPost>(`${this.apiUrl}/posts`, payload);
+  };
+
+  deletePost = (postId: number) => {
+    return this.http.delete<void>(`${this.apiUrl}/posts/${postId}`);
+  };
+}
