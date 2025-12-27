6671830c599c6291d29f3a17e61056fc4b09a291
Prajwal Sharma <prjwl4@gmail.com>
2025-12-27 21:18:51 +0530

    feat: add home page component to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home-component/home-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('angular-crud-tutorial');
-}
+export class App {}
