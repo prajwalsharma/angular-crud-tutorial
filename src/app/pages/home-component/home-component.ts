05c133a2c5ba23b995d42e36450e45d78d7c8e22
Prajwal Sharma <prjwl4@gmail.com>
2025-12-27 21:21:09 +0530

    feat: show post list on home page

--- src/app/pages/home-component/home-component.ts
+++ src/app/pages/home-component/home-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PostListComponent } from '../../components/post-list-component/post-list-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [PostListComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
